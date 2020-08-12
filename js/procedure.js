22bd2622eeed95ca66e437ad959f6ce24d37ad21
Kelden Ben-Ora <kjbenora@ucdavis.edu>
2020-08-12 12:00:08 -0700

    fixed end trial highlight error

--- js/procedure.js
+++ js/procedure.js
@@ -643,7 +643,7 @@ function buildProcedure(steps, parent, depth) {
     } else if (step.type == 'caution') {
       $('<li id="' + procID + '"><div class="caution">' + step.text + '</div></li>').appendTo('ul#' + parent);
     } else if (step.type == 'button') {
-      $('<li><button type="button" onclick="end()" id="end">' + step.text + '</button></li>').appendTo('ul#' + parent);
+      $('<li id="' + procID + '"><button type="button" onclick="end()" id="end">' + step.text + '</button></li>').appendTo('ul#' + parent);
     } else {
       step.image = (step.image === undefined) ? '' : step.image;
 
